817108d4fb7a9754620845adf4bcc68e2b51c7ae
TucoT9 <admin@tucot9.com>
2024-11-01 20:45:03 +0100

    Update cheat.lua
    bessere farben

--- cheat.lua
+++ cheat.lua
@@ -1,5 +1,5 @@
---[[
-    Made by TucoT9
+--[[ 
+    Made by TucoT9 
 ]]
 
 -- GUI-Erstellung für NeonScripting
@@ -60,7 +60,7 @@ esp.Players = false
 esp:AddObjectListener(workspace, {
    Name = "soldier_model",
    Type = "Model",
-   Color = Color3.fromRGB(255, 0, 4),
+   Color = Color3.fromRGB(128, 0, 128),  -- Leuchtendes Lila
 
    -- Primäres Teil des Modells als HumanoidRootPart festlegen
    PrimaryPart = function(obj)
@@ -165,3 +165,4 @@ game.StarterGui:SetCore("SendNotification", {
    Icon = "",
    Duration = 5
 })
+
